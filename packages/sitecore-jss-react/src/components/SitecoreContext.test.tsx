4ef63e613f0d2f35ef72cd95e1f80cc212717675
Yavor Krastev <4502045+yavorsk@users.noreply.github.com>
2025-05-30 17:51:36 +0300

    [sitecore-jss] [sitecore-jss-react] [sitecore-jss-react-forms] [sitecore-jss-nextjs] [create-sitecore-jss] [sitecore-jss-proxy] [sitecore-jss-angular] Upgrade React to version 19 and Nextjs to version 15 (#2095)
    Co-authored-by: Artem Alexeyenko <ala@sitecore.net>
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>
    (cherry picked from commit 3aae2e35053278731eae58b2f4fd233b24ef9bc0)

--- packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
+++ packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
@@ -7,14 +7,6 @@ import { WithSitecoreContextProps, withSitecoreContext } from '../enhancers/with
 import { LayoutServiceData, SitecoreContextValue } from '../index';
 import { render } from '@testing-library/react';
 import { useSitecoreContext } from '../enhancers/withSitecoreContext';
-import { language } from '@sitecore-cloudsdk/core/internal';
-
-const api = {
-  edge: {
-    contextId: 'id',
-    edgeUrl: 'url',
-  },
-};
 
 describe('SitecoreContext', () => {
   let nestedContext = {};
@@ -76,13 +68,6 @@ describe('SitecoreContext', () => {
     );
 
     expect(contextApi).to.deep.equal({
-      edge: {
-        contextId: 'id',
-        edgeUrl: 'url',
-      },
-    });
-
-    expect(component.state().api).to.deep.equal({
       edge: {
         contextId: 'id',
         edgeUrl: 'url',
